64cffee5cf9424eaacb3ecfacb272e7625a18f32
Krishna Nand Dubey <krishnanand025@gmail.com>
2024-02-13 20:03:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import io from "socket.io-client";
+import Chat from "./Chat";
+
+const socket = io.connect("chat-app-server-production-4216.up.railway.app");
+
+const App=()=> {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        backgroundColor="#f0f0f0" 
+      >
+        {!showChat ? (
+          <Paper sx={{ padding: 4, width: 300, backgroundColor: "#fff" }}> 
+            <Typography variant="h4" align="center" fontFamily="Montserrat" color="#4CAF50">
+              Enter the Chat Zone
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                marginTop: 2,
+              }}
+            >
+              <TextField
+                variant="outlined"
+                label="Username"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+              <TextField
+                variant="outlined"
+                label="Room Id"
+                onChange={(event) => {
+                  setRoom(event.target.value);
+                }}
+              />
+              <Button
+                onClick={joinRoom}
+                variant="contained"
+                color="primary"
+                fullWidth
+                style={{ backgroundColor: "#4CAF50", color: "#fff" }} 
+              >
+                Enter Room
+              </Button>
+            </Box>
+          </Paper>
+        ) : (
+          <Chat socket={socket} username={username} room={room} setShowChat={setShowChat} />
+        )}
+      </Box>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Chat.js
@@ -0,0 +1,238 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import ScrollToBottom from "react-scroll-to-bottom";
+import SendIcon from "@mui/icons-material/Send";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import "./App.css";
+
+const Chat=({ socket, username, room, setShowChat }) =>{
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messageList]);
+  const sendMessage = async () => {
+    if (currentMessage.trim() !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage.trim(),
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    const receiveMessage = (data) => {
+      setMessageList((list) => [...list, data]);
+    };
+
+    socket.on("receive_message", receiveMessage);
+
+    return () => {
+      socket.off("receive_message", receiveMessage);
+    };
+  }, [socket]);
+
+  const leaveRoom = () => {
+    setShowChat(false);
+  };
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <>
+    {isMobile ? (
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          padding: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="body1"  sx={{ marginRight: 1 }}>
+          Hello, {username}
+        </Typography>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={leaveRoom}
+          sx={{ textTransform: "none" }}
+        >
+          Leave Room
+        </Button>
+      </Box>
+    ) : (
+      <Box
+        sx={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          padding: 2,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="body1" sx={{ marginRight: 1 }}>
+          Hello, {username}
+        </Typography>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={leaveRoom}
+          sx={{ textTransform: "none" }}
+        >
+          Leave Room
+        </Button>
+      </Box>
+    )}
+    <Paper
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: 500,
+        width: 300,
+        marginTop: 2,
+        padding: 5,
+      }}
+    >
+      <Typography variant="h5" align="center" gutterBottom mb={3}>
+        Chat Messages
+      </Typography>
+
+      <Box sx={{ flex: 1, overflowY: "auto" ,paddingX:1}}>
+        <ScrollToBottom>
+          {messageList.map((messageContent, index) => (
+            <Box
+              key={index}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems:
+                  messageContent.author === username
+                    ? "flex-end"
+                    : "flex-start",
+                mb: 1,
+              }}
+            >
+              {index === 0 ||
+              messageContent.author !== messageList[index - 1].author ? (
+                <Box
+                  sx={{
+                    textAlign: "left",
+                  }}
+                >
+                  <Typography variant="subtitle2" fontWeight="bold">
+                    {messageContent.author === username
+                      ? "You"
+                      : messageContent.author}
+                  </Typography>
+                </Box>
+              ) : null}
+              <Box
+                sx={{
+                  backgroundColor:
+                    messageContent.author === username
+                      ? "#1fcbdde3"
+                      : "#d2d2d18a",
+                  borderRadius: 1,
+                  p: 1,
+                  maxWidth: "70%",
+                  display: "flex",
+                  flexDirection: "column",
+                 
+                  '&:before': (messageContent.author === username) && (index === 0 ||
+                  messageContent.author !== messageList[index - 1].author) ?{
+                    content: '""',
+                    position: 'absolute',
+                    right:'-8px',
+                    borderBottom: '10px solid transparent',
+                    borderLeft: '11px solid #1fcbdde3',
+                    transform: 'translateY(-80%)',
+
+                  }:(index === 0 ||
+                  messageContent.author !== messageList[index - 1].author) &&{
+                    content: '""',
+                    position: 'absolute',
+                    left:'-8px',
+                    borderBottom: '10px solid transparent',
+                    borderRight: '11px solid #d2d2d18a',
+                    transform: 'translateY(-80%)',
+
+                  }
+                }}
+              >
+                <Typography variant="body2" color="textSecondary">
+                  {messageContent.message}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  sx={{
+                    fontSize: 9,
+                    marginTop: "auto",
+                    textAlign: "right",
+                  }}
+                >
+                  {messageContent.time}
+                </Typography>
+                {index === messageList.length - 1 && <div ref={messagesEndRef} />}
+              </Box>
+            </Box>
+          ))}
+        </ScrollToBottom>
+      </Box>
+      <Box sx={{ mt: 1, display: "flex" }}>
+        <TextField
+          type="text"
+          value={currentMessage}
+          label="Type a Message"
+          variant="standard"
+          sx={{ flexGrow: 1, mr: 1 }}
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <IconButton onClick={sendMessage} color="primary">
+          <SendIcon />
+        </IconButton>
+      </Box>
+    </Paper>
+    </>
+  );
+}
+
+export default Chat;
